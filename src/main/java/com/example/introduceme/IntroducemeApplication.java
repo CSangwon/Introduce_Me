11c27677f8aa6fb131c1637209aab74fd0ddbdfd
sangwon <swchoi1997@naver.com>
2022-06-28 23:28:46 +0900

    first commit

---
+++ src/main/java/com/example/introduceme/IntroducemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.introduceme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroducemeApplication.class, args);
+    }
+
+}
